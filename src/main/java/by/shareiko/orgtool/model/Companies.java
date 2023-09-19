4f74f10bfedcd91b7ca08f2c8bcc6a9e6d609cc8
Pavel Shareika <pavel.shareiko@gmail.com>
2023-09-19 08:55:10 +0300

    remove accessors for inner collections in Employees and Companies models

--- src/main/java/by/shareiko/orgtool/model/Companies.java
+++ src/main/java/by/shareiko/orgtool/model/Companies.java
@@ -1,15 +1,32 @@
 package by.shareiko.orgtool.model;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Data
 @XmlRootElement(name = "companies")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Companies {
     @XmlElement(name = "company")
-    private List<Company> companies;
+    @Getter(AccessLevel.NONE)
+    private List<Company> companies = new ArrayList<>();
+
+    public boolean add(Company company) {
+        return this.companies.add(company);
+    }
+
+    public boolean remove(Company company) {
+        return this.companies.remove(company);
+    }
+
+    public Stream<Company> stream() {
+        return companies.stream();
+    }
 }
--- src/main/java/by/shareiko/orgtool/model/Employees.java
+++ src/main/java/by/shareiko/orgtool/model/Employees.java
@@ -1,17 +1,34 @@
 package by.shareiko.orgtool.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Data
 @XmlRootElement(name = "employees")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Employees {
     @XmlElement(name = "employee")
-    private List<Employee> employees;
+    @Getter(AccessLevel.NONE)
+    private List<Employee> employees = new ArrayList<>();
+
+    public boolean add(Employee employee) {
+        return this.employees.add(employee);
+    }
+
+    public boolean remove(Employee employee) {
+        return this.employees.remove(employee);
+    }
+
+    public Stream<Employee> stream() {
+        return this.employees.stream();
+    }
 }
